18a46b14a9b9f5e028d7d028e3c9fe402ee41c3f
c-ejinn <84281094+c-ejinn@users.noreply.github.com>
2023-05-12 17:24:42 +0800

    blub blub blub

---
+++ main.js
@@ -0,0 +1,17 @@
+// const url = 'descText.json';
+// let descTexts;
+
+
+// function reqData() {
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => adder(data))
+//         .catch((error) => console.log(error));
+// }
+
+// function adder(data) {
+//     descTexts = data;
+//     //console.log(descTexts);
+// }
+
+// reqData();
